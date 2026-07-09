a0df41f5baa00d08d0e56c1760a5341cde9ca0de
iki1uc <entertrainer@web.de>
2026-07-09 11:22:20 +0200

    Update forward.js

--- AX/forward.js
+++ AX/forward.js
@@ -1,6 +1,29 @@
-// AX: Forward-Move
-export function AX_forward() {
-    console.log("AX → forward");
-    return "AX_FORWARD_OK";
-}
+// Achsen laden
+loadHTML("NUL/0.html", "#axis");
+loadHTML("BIR/1.html", "#axis");
+loadHTML("UC/3.html", "#axis");
+loadHTML("MA3/2.html", "#axis");
 
+// Stationen laden
+loadHTML("ASTROLOGIE/comet.html", "#astro");
+loadHTML("ASTROLOGIE/einfluss.html", "#astro");
+loadHTML("ASTROLOGIE/muster.html", "#astro");
+loadHTML("ASTROLOGIE/resonanz.html", "#astro");
+loadHTML("ASTROLOGIE/sternzeichen.html", "#astro");
+
+loadHTML("ASTRONOMIE/kosmos.html", "#astron");
+loadHTML("ASTRONOMIE/planeten.html", "#astron");
+loadHTML("ASTRONOMIE/schwarzes_loch.html", "#astron");
+loadHTML("ASTRONOMIE/systeme.html", "#astron");
+loadHTML("ASTRONOMIE/umlauf.html", "#astron");
+
+loadHTML("HOROSKOPE/GEBtg.html", "#hor");
+loadHTML("HOROSKOPE/chinesisch.html", "#hor");
+loadHTML("HOROSKOPE/interessen.html", "#hor");
+loadHTML("HOROSKOPE/jahres.html", "#hor");
+loadHTML("HOROSKOPE/tages.html", "#hor");
+
+// META laden
+loadHTML("META/comet.html", "#meta");
+loadHTML("META/kosmos.html", "#meta");
+loadHTML("META/systeme.html", "#meta");
